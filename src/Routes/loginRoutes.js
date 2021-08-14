96fe7de0d125c03027b7bb75f404be8fa767a17d
Andrea Pullen <70964079+apullenb@users.noreply.github.com>
2021-08-14 16:01:55 -0500

    update login route

--- src/Routes/loginRoutes.js
+++ src/Routes/loginRoutes.js
@@ -9,61 +9,62 @@ const router = express.Router();
 
 //Registration Route: Create a New User
 router
-  .post("/add-user", jsonParser, async (req, res, next) => {
-    const { name, username, password, email } = req.body;
-    const newUser = {
-      username,
-      password,
-      name,
-      email,
-    };
+.post("/add-user", jsonParser, async (req, res, next) => {
+  const { name, username, password, email } = req.body;
+  const newUser = {
+    username,
+    password,
+    name,
+    email,
+  };
 
-    try {
-      const userCheck = await db.findUser(newUser.username);
-      if (userCheck.length > 0) {
-        return res.status(400).json({ error: "username not available" });
-      }
-      const saltRound = 3;
-      const salt = await bcrypt.genSalt(saltRound);
-      const bcryptPassword = await bcrypt.hash(newUser.password_hash, salt);
-      newUser.password_hash = bcryptPassword;
-      const addUser = await db.addNewUser(newUser);
-
-      res.status(201).json("Account Created Successfully");
-    } catch (error) {
-      console.error(error);
-      res.status(500).json(error);
+  try {
+    const userCheck = await db.findUser(newUser.username);
+    if (userCheck.length > 0) {
+      return res.status(400).json({ error: "username not available" });
     }
-  })
+    const saltRound = 3;
+    const salt = await bcrypt.genSalt(saltRound);
+    const bcryptPassword = await bcrypt.hash(newUser.password_hash, salt);
+    newUser.password_hash = bcryptPassword;
+    const addUser = await db.addNewUser(newUser);
 
-  // Login Route:
-  .post("/login", jsonParser, async (req, res, next) => {
-    const { username, password } = req.body;
-    const loginUser = { username, password_hash: password };
+    res.status(201).json("Account Created Successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+})
 
-    for (const [key, value] of Object.entries(loginUser))
-      if (value == null)
-        return res.status(400).json({
-          error: `Missing '${key}' in request body`,
-        });
-    const searchUsers = await db.findUser(loginUser.username);
-    if (searchUsers === undefined) {
+// Login Route:
+.post("/login", jsonParser, async (req, res, next) => {
+  const { username, password } = req.body;
+  const loginUser = { username, password };
+
+  for (const [key, value] of Object.entries(loginUser))
+    if (value == null)
       return res.status(400).json({
-        error: "Incorrect username or password",
+        error: `Missing '${key}' in request body`,
       });
-    }
-    return db
-      .comparePasswords(loginUser.password_hash, searchUsers[0].passwordHash)
-      .then((compareMatch) => {
-        if (!compareMatch) {
-          return res.status(400).json({
-            error: "Incorrect username or password",
-          });
-        }
-        const token = jwtGenerator(searchUsers[0].id);
-        res.json({ token, id: searchUsers[0].id });
-      })
-      .catch(next);
-  });
+  const searchUsers = await db.findUser(loginUser.username);
+  if (searchUsers === undefined) {
+    return res.status(400).json({
+      error: "Incorrect username or password",
+    });
+  }
+  return db
+    .comparePasswords(loginUser.password, searchUsers[0].password)
+    .then((compareMatch) => {
+      if (!compareMatch) {
+        return res.status(400).json({
+          error: "Incorrect username or password",
+        });
+      }
+      const token = jwtGenerator(searchUsers[0].id);
+      res.json({ token, id: searchUsers[0].id });
+    })
+    .catch(next);
+});
 
-  module.exports = router;
+module.exports = router;
+
